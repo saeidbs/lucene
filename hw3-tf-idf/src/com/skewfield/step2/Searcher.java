940769d5bf411f51eb036ac1fcde3e9d009320f5
saeid <saeidbahmanisangesari@mail.um.ac.ir>
2019-06-27 00:15:13 +0430

    add for tf-df

--- hw3-tf-idf/src/com/skewfield/step2/Searcher.java
+++ hw3-tf-idf/src/com/skewfield/step2/Searcher.java
@@ -6,12 +6,10 @@ import java.io.IOException;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
@@ -36,7 +34,18 @@ public class Searcher {
             throws IOException, ParseException {
         query = queryParser.parse(searchQuery);
 
-
+//        PhraseQuery phraseQuery = new PhraseQuery();
+//
+//        String[] strings=searchQuery.split(" ");
+//        for (int i=0;i<strings.length;i++) {
+//            phraseQuery.add(new Term(LuceneConstants.CONTENTS, strings[i]), i);
+//
+//        }
+//        phraseQuery.setSlop(1000);
+//
+//
+//
+//        return indexSearcher.search(phraseQuery, LuceneConstants.MAX_SEARCH);
         return indexSearcher.search(query, LuceneConstants.MAX_SEARCH);
     }
 
